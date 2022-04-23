2f2bd5afe63c115349f8d3697a33297b479e970b
brice <briceguard-git@yahoo.com>
2022-04-23 16:31:06 +0100

    moved to consistent package and git repo name

--- Package.swift
+++ Package.swift
@@ -16,14 +16,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:roznet/Geomagnetism-Swift.git", from: "1.0.0"),
+        .package(url: "git@github.com:roznet/Geomagnetism.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RZFlight",
-            dependencies: [ .product(name: "Geomagnetism", package: "Geomagnetism-Swift") ] ),
+            dependencies: [ .product(name: "Geomagnetism", package: "Geomagnetism") ] ),
         .testTarget(
             name: "RZFlightTests",
             dependencies: ["RZFlight"]),
