95119cbfd92c4975d2d1a5c4a078b1320c8bde04
brice <briceguard-github@yahoo.com>
2024-01-27 10:19:52 +0100

    point of entry kml with colors

--- python/exportForeflight.py
+++ python/exportForeflight.py
@@ -26,66 +26,75 @@ def buildPPF():
 
     kml = simplekml.Kml()
     iconurl = 'https://www.gstatic.com/mapspro/images/stock/503-wht-blank_maps.png'
-    iconurl = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
+    #iconurl = 'http://maps.google.com/mapfiles/kml/shapes/placemark_circle.png'
 
     icao_to_color = {
-        "LF": "#EEEEEE",  # France (White)
-        "EG": "#021f66",  # United Kingdom (Red)
-        "ED": "#f6c800",  # Germany (Black)
-        "LE": "#FF6600",  # Spain (Orange)
-        "LI": "#009246",   # Italy (Green)
-        "EH": "#AE1C28", # Netherlands (Maroon)
-        "EB": "#000000",   # Belgium (Black)
-        "LS": "#f70000",   # Switzerland (Red)
-        "LO": "#d81013",   # Austria (Red)
-        "ES": "#0051BA",   # Sweden (Blue)
-        "EN": "#EF3340",   # Norway (Crimson)
-        "EK": "#C60C30",   # Denmark (Red)
-        "EF": "#003580",   # Finland (Blue)
-        "LG": "#0D5EAF",   # Greece (Blue)
-        "LP": "#009639",   # Portugal (Green)
-        "EI": "#039547",    # Ireland (Green)
-        "EP": "#DC143C",   # Poland (Crimson)
-        "LH": "#ED3340",   # Hungary (Vivid Red)
-        "LK": "#D41E35",   # Czech Republic (Red)
-        "LZ": "#D52B1E",   # Slovakia (Red)
-        "LJ": "#FFFFFF",   # Slovenia (White)
-        "LD": "#003DAA",   # Croatia (Blue)
-        "LR": "#0055A4",   # Romania (Blue)
-        "LB": "#008751",   # Bulgaria (Green)
-        "LT": "#E30A17",   # Turkey (Red)
+        "LF": simplekml.Color.white,  # France (White)
+        "EG": simplekml.Color.red,  # United Kingdom (Red)
+        "ED": simplekml.Color.black,  # Germany (Black)
+        "LE": simplekml.Color.orange,  # Spain (Orange)
+        "LI": simplekml.Color.green,   # Italy (Green)
+        "EH": simplekml.Color.maroon, # Netherlands (Maroon)
+        "EB": simplekml.Color.black,   # Belgium (Black)
+        "LS": simplekml.Color.red,   # Switzerland (Red)
+        "LO": simplekml.Color.white,   # Austria (Red)
+        "ES": simplekml.Color.yellow,   # Sweden (Blue)
+        "EN": simplekml.Color.blue,   # Norway (Crimson)
+        "EK": simplekml.Color.red,   # Denmark (Red)
+        "EF": simplekml.Color.white,   # Finland (Blue)
+        "LG": simplekml.Color.blue,   # Greece (Blue)
+        "LP": simplekml.Color.green,   # Portugal (Green)
+        "EI": simplekml.Color.green,    # Ireland (Green)
+        "EP": simplekml.Color.crimson,   # Poland (Crimson)
+        "LH": simplekml.Color.red,   # Hungary (Vivid Red)
+        "LK": simplekml.Color.red,   # Czech Republic (Red)
+        "LZ": simplekml.Color.red,   # Slovakia (Red)
+        "LJ": simplekml.Color.white,   # Slovenia (White)
+        "LD": simplekml.Color.blue,   # Croatia (Blue)
+        "LR": simplekml.Color.blue,   # Romania (Blue)
+        "LB": simplekml.Color.green,   # Bulgaria (Green)
+        "LT": simplekml.Color.red,   # Turkey (Red)
     }
+
 
     lat_offset = 0.0045  # about 500m
     count={}
+
+
+    styles = {}
+    smallFile = False
     for (one,row) in map.items():
         identprefix = row['ident'][:2]
         if identprefix not in count:
             count[identprefix] = 0
             if identprefix not in icao_to_color:
                 print(f'Unknown {identprefix}')
+                stylesmap = simplekml.StyleMap()
+                stylesmap.normalstyle.labelstyle.color = simplekml.Color.blue
+                stylesmap.normalstyle.iconstyle.color = simplekml.Color.blue
+                stylesmap.normalstyle.iconstyle.icon.href=iconurl
+                stylesmap.highlightstyle.iconstyle.color = simplekml.Color.red
+                stylesmap.highlightstyle.labelstyle.color = simplekml.Color.red
+                stylesmap.highlightstyle.iconstyle.icon.href=iconurl
             else:
                 print(f'Prefix  {identprefix} {icao_to_color[identprefix]}')
+                stylesmap = simplekml.StyleMap()
+                stylesmap.normalstyle.labelstyle.color = icao_to_color[identprefix]
+                stylesmap.normalstyle.iconstyle.color = icao_to_color[identprefix]
+                stylesmap.normalstyle.iconstyle.icon.href=iconurl
+                stylesmap.highlightstyle.iconstyle.color = icao_to_color[identprefix]
+                stylesmap.highlightstyle.labelstyle.color = icao_to_color[identprefix]
+                stylesmap.highlightstyle.iconstyle.icon.href=iconurl
+            styles[identprefix] = stylesmap
+
         count[identprefix] += 1
-        if False and count[identprefix] > 2:
+        if smallFile and count[identprefix] > 2:
             continue
 
         p = kml.newpoint(name='POE.'+row['ident'])
         p.coords = [(row['longitude'], row['latitude']+lat_offset)]
-        if row['ident'][:2] in icao_to_color:
-            c = icao_to_color[row['ident'][:2]]
-            p.stylemap.normalstyle.labelstyle.color = c
-            p.stylemap.normalstyle.iconstyle.icon.href=iconurl
-            p.stylemap.normalstyle.iconstyle.color = c
-            p.stylemap.highlightstyle.iconstyle.color = simplekml.Color.red
-            p.stylemap.highlightstyle.labelstyle.color = simplekml.Color.red
-        else:
-            p.stylemap.normalstyle.iconstyle.icon.href=iconurl
-            p.stylemap.normalstyle.labelstyle.color = simplekml.Color.blue
-            p.stylemap.highlightstyle.labelstyle.color = simplekml.Color.red
-            p.stylemap.normalstyle.iconstyle.color = simplekml.Color.blue
-            p.stylemap.highlightstyle.iconstyle.color = simplekml.Color.red
-
+        stylemap = styles[identprefix]
+        p.stylemap = stylemap
 
     kml.save('PointOfEntry.kml')
 buildPPF()
